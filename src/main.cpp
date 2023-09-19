b1b6119eae9ff8a6bf03c900fbb1c550844bb412
Pedro Dias <phpdias@outlook.com>
2023-09-19 09:33:17 -0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -18,21 +18,17 @@ int main() {
         unordered_set<string> stopwords = readStopwords("./dataset/stopwords/stopwords.txt");
         openTextFile(filePath, userFile);
         processText(userFile, wordsToSearch, stopwords);
-        //processHash(wordsToSearch, k, filePath);
         userFile.close();
     }
 
     ofstream outFile("./dataset/outputs/output.txt", ios::app);
 
     for(const string &filePath : filePaths){
-        int wordCount;
         ifstream inputFile;
         unordered_map<string, int> frequencyMap;
         unordered_set<string> stopwords = readStopwords("./dataset/stopwords/stopwords.txt");
         openTextFile(filePath, inputFile);
         processText(inputFile, frequencyMap, stopwords);
-        // processHash(frequencyMap, k, filePath);
-        wordCount = countWords(frequencyMap, wordsToSearch);
         priority_queue<HeapNode, vector<HeapNode>, MinHeapComparator> minHeap = processHash(frequencyMap, k, filePath);
         printWordFrequency(frequencyMap, wordsToSearch, filePath, outFile);
         printMinHeap(filePath, minHeap, outFile, wordsToSearch, frequencyMap, k);
